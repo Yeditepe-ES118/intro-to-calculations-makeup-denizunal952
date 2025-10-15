fe682a2cf883b87680c628759ec716dd6f772fe2
denizunal1952 <denizunal@Deniz-Dizustu-Bilgisayar.local>
2025-10-15 20:06:07 +0300

    add lab3 documents

--- lab3_problem1.py
+++ lab3_problem1.py
@@ -1,8 +1,13 @@
 import numpy as np
-def triangle(x,y):
-    theta = np.arctan(x/y) * 180 / np.pi #in degrees
-    gamma = np.arctan(y/x) * 180 / np.pi #in degrees
-    d = np.sqrt(x**2 + y**2)
-    s = 1 / np.cos(theta * np.pi / 180)
-    return theta, gamma, d, s
-myresult = triangle(3,4)
+def surface_area(pi_app):
+    a = 6378.137 #in km
+    c = 6356.752314245 #in km
+    e_squared = 1 - c**2 / a**2
+    s = 2* pi_app * a**2 * (1 + (1 - e_squared) / np.sqrt(e_squared) + np.arctanh(np.sqrt(e_squared)))
+    return s
+def compute_error(pi_app1, pi_app2):
+    S_1 = surface_area(pi_app1)
+    S_2 = surface_area(pi_app2)
+    error = np.abs(S_1 - S_2) / S_2 * 100 
+    return error
+myresult = compute_error(3.14,3.1415)
